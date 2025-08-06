9cc7c16be10571fd12df0ace03b0716543834755
DOYDEV1982 <oladoyejelili53@yahoo.com>
2025-08-06 07:41:02 +0100

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from "./Pages/HomePages";
-import AboutPage from "./pages/AboutPage";
+import AboutPage from "./Pages/AboutPage";
 import SkillPage from "./Pages/SkillPage";
-import ProjectPage from "./pages/ProjectPage";
-import ContactPage from "./pages/ContactPage";
+import ProjectPage from "./Pages/ProjectPage";
+import ContactPage from "./Pages/ContactPage";
 import Resume from "./components/Resume";
-import ServicePage from "./pages/ServicePage";
+import ServicePage from "./Pages/ServicePage";
 
 
 
